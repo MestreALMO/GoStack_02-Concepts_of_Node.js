8ce8a10bf373a90b9010678b52119d039519543d
MestreALMO <lusegardis@gmail.com>
2020-08-18 00:07:18 -0300

    completed: POST & GET /repositories

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,15 +10,40 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title: 'Desafio Node.js', url: 'http://github.com/...', techs: ["Node.js", "..."], likes: 0 };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  // const { id } = request.params;
+  // const { title, url, techs } = request.body;
+
+  // const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  // if (repositoryIndex < 0) {
+  //   return response.status(400).json({ error: "Project not found." });
+  // }
+
+  // const repository = {
+  //   id,
+  //   title,
+  //   url,
+  //   techs,
+  //   likes,
+  // };
+
+  // repositories[repositoryIndex] = repository;
+
+  // return reponse.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
