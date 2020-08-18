37d0867f7d475cdbde845368f2e2923c20cc2a3c
MestreALMO <lusegardis@gmail.com>
2020-08-18 01:04:39 -0300

    PUT method fully working

--- src/app.js
+++ src/app.js
@@ -24,26 +24,28 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // const { id } = request.params;
-  // const { title, url, techs } = request.body;
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
 
-  // const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  // if (repositoryIndex < 0) {
-  //   return response.status(400).json({ error: "Repository not found." });
-  // }
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not found." });
+  }
 
-  // const repository = {
-  //   id,
-  //   title,
-  //   url,
-  //   techs,
-  //   likes,
-  // };
+  const exemplo = repositories[repositoryIndex];
 
-  // repositories[repositoryIndex] = repository;
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: exemplo.likes
+  };
 
-  // return response.json(repository);
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
